f979ca23d6c4945b6036efac50d988f5bb3ccc7f
Sunil Kumar Korlapu <korlapusunilkumar@gmail.com>
2022-10-20 00:49:30 +0530

    updated the tf var

--- environment/variables.tf
+++ environment/variables.tf
@@ -8,13 +8,6 @@ variable "subnet_id_1" {
   default = "subnet-02490d3daef28b8a3"
  }
 
-
-variable "subnet_id_1" {
-  type = string
-  default = "subnet-0389a0520e6d7ed85"
-  sensitive = true
- }
- 
  variable "devops_access_key" {}
  
  variable "devops_secret_key" {}
