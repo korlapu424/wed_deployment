c9b6cf328f23d64944831b715eb2a86551bbc6b2
Sunil Kumar Korlapu <31651139+korlapu424@users.noreply.github.com>
2023-03-21 23:53:34 +0530

    Update variables.tf

--- environment/variables.tf
+++ environment/variables.tf
@@ -1,11 +1,11 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-0389a0520e6d7ed85"
+  default = "subnet-085585c47a8cedf3f5"
  }
  
  variable "subnet_id_2" {
   type = string
-  default = "subnet-02490d3daef28b8a3"
+  default = "subnet-045e3662ec14c2ead"
  }
 
  variable "devops_access_key" {}
